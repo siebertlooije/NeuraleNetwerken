73d5080601efd4d82642ace99c3b1c84619a16ea
Sybren Römer <sybren.romer@gmail.com>
2017-01-01 18:17:54 +0100

    Poging tot zo ver

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,29 +1,19 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io
+
+# def check_E(E, c):
+#     for i in range(0, len(E)):
+#         if (E[i] <= c):
+#             return False
+#     return True
+
 
 def generate_label(percent=50):
     return 1 if random.randrange(0, 100) > percent else -1
-
-
-def generate_dataset(P, N):
-    """
-    :param P: Number of examples
-    :param N: Number of dimensions for the feature
-    :return: the random generated dataset
-    """
-
-    ID = []
-    for i in range(0, P):
-        eps = []
-        for j in range(0, N):
-            eps.append(random.gauss(0, 1))
-
-        S = generate_label()
-        ID.append([eps, S])
-    return ID
 
 
 def load_dataset():
@@ -31,13 +21,6 @@ def load_dataset():
     xi = np.transpose(mat['xi'])
     tau = mat['tau']
     return (xi, tau)
-
-
-def check_E(E, c):
-    for i in range(0, len(E)):
-        if (E[i] <= c):
-            return False
-    return True
 
 
 def Feed_forward(w1,w2,eps):
@@ -68,7 +51,7 @@ def select_random_example(len_examples):
     return np.random.randint(len_examples, size = 1)[0]
 
 
-def seq_training(ID, P, n, N, c):
+def seq_training(ID, tau, P, n, N):
     """
     :param ID: the dataset
     :param P: the number of examples
@@ -82,70 +65,45 @@ def seq_training(ID, P, n, N, c):
         cost_function = sto_gradient_descent(w1,w2,P,ID)
         random_example = P[select_random_example(len(P))]
         w1,w2 = calculate_weights(w1,w2,ID,random_example,n)
-    return cost_function
-
-
-def plot_c():
-    Qts = []
-    N = 20  # Number of features
-    nD = 50  # Number of generated dataset
-    n = 100  # Number of epoch
-    alphas = np.arange(0.75, 3.25, 0.25)
-    for c in [-0.1, -0.05, 0, 0.05, 0.1]:
-        Qts = []
-        for alpha in alphas:
-            P = int(alpha * N)  # Number of examples
-
-            succes_counter = 0.0
-            counter = 0.0
-            for i in range(2, nD):
-                ID = generate_dataset(P, N)
-                w, succes = seq_training(ID, P, n, N, c)
-
-                if (succes):
-                    succes_counter += 1
-                counter += 1
-            print(float(succes_counter / counter))
-            Qts.append(float(succes_counter / counter))
-
-        plt.plot(alphas, Qts, label=str(c) + " C")
-    plt.xlabel('Alpha')
-    plt.ylabel('Succes rate')
-    axes = plt.gca()
-    axes.set_ylim([-0.2, 1.2])
-    plt.legend()
-    plt.show()
+        cost_functions.append(cost_function)
+    return cost_functions
 
 
 def plot_different_parameters():
-    Qts = []
-    N = 20  # Number of features
-    nD = 50  # Number of generated dataset
+    P = 100 # Number of training samples
+    Q = 100 # Number of test samples
+    eta = 0.05 # Learning rate
     n = 100  # Number of epoch
+    N = 50
 
-    alphas = np.arange(0.75, 3.25, 0.25)
-    for N in [10, 20, 50, 100, 200]:
-        Qts = []
-        for alpha in alphas:
-            P = int(alpha * N)  # Number of examples
+    xi, tau = load_dataset()
+    train_samples = xi[0:P] # First P examples for training
+    test_samples = xi[P:P+Q] # Q examples after that for testing
 
-            succes_counter = 0.0
-            counter = 0.0
-            for i in range(2, nD):
-                ID = generate_dataset(P, N)
-                w, succes = seq_training(ID, P, n, N)
+    result = seq_training(train_samples, tau, P, n, N)
 
-                if (succes):
-                    succes_counter += 1
-                counter += 1
-            print(float(succes_counter / counter))
-            Qts.append(float(succes_counter / counter))
 
-        plt.plot(alphas, Qts, label=str(N) + " features")
-    plt.legend()
-    plt.ylabel('Succes rate')
-    plt.xlabel('alpha')
-    plt.title('Succes rate for different alphas and dimensions')
+
+    # alphas = np.arange(0.75, 3.25, 0.25)
+    # for alpha in alphas:
+    #     P = int(alpha * N)  # Number of examples
+
+    #     succes_counter = 0.0
+    #     counter = 0.0
+    #     for i in range(2, nD):
+    #         ID = generate_dataset(P, N)
+    #         w, succes = seq_training(ID, P, n, N)
+
+    #         if (succes):
+    #             succes_counter += 1
+    #         counter += 1
+    #     print(float(succes_counter / counter))
+    #     Qts.append(float(succes_counter / counter))
+
+    plt.plot(result)
+    plt.ylabel(' ')
+    plt.xlabel(' ')
+    plt.title('Cost vs time')
     plt.show()
 
 
