be88d361cd0ba81fe67a6e9781a8b0b3974a6177
Siebert Looije <s.looije@anchormen.nl>
2017-01-02 12:56:03 +0100

    changes the code so it works now

--- assignment3/main.py
+++ assignment3/main.py
@@ -29,21 +29,19 @@ def Feed_forward(w1,w2,eps):
 
 #e = ((feedforward() - label)^2)/2
 #w = w - learning_step * gradient(e)
-def calculate_weights(w1,w2, IDs, example, learning_step):
-    e = ((Feed_forward(w1,w2,example) - IDs[example])^2)*0.5
+def calculate_weights(w1,w2, train_sample,train_tau, learning_step):
+    e = np.sqrt(np.power(Feed_forward(w1,w2,train_sample) - train_tau,2))
     w1_new = w1 - learning_step * np.gradient(w1)*e
     w2_new = w2 - learning_step * np.gradient(w2)*e
     return w1_new,w2_new
 
-
 #E = 1/p * 1/2 for all examples do : (feed_forward(eps ) - label(eps))^2
-def sto_gradient_descent(w1,w2,examples, IDs):
+def sto_gradient_descent(w1,w2, samples, tau):
     sum_grad = 0
-    for example in examples:
-        sum_grad += (Feed_forward(w1,w2,example) - IDs(example))^2
-
-    #0.5/len(examples) = (1/2)*(1/len(examples)
-    final_grad = 0.5/len(examples)*sum_grad
+    for index,sample in enumerate(samples):
+        result = np.power(Feed_forward(w1,w2,sample) - tau[index],2)
+        sum_grad += result
+    final_grad = 0.5/len(samples)*sum_grad
     return final_grad
 
 
@@ -51,22 +49,30 @@ def select_random_example(len_examples):
     return np.random.randint(len_examples, size = 1)[0]
 
 
-def seq_training(ID, tau, P, n, N):
+def seq_training(train_samples, test_samples, train_tau,test_tau, P, n, N):
     """
     :param ID: the dataset
     :param P: the number of examples
     :param n: the number of epoches
     :return:
     """
-    w1 = np.random.uniform(N)
-    w2 = np.random.uniform(N)
+    w1 = np.random.uniform(-1,1,N)
+    w2 = np.random.uniform(-1,1,N)
+
     cost_function = 0
+    cost_train = []
+    cost_test = []
     for epoch in range(0, n):
-        cost_function = sto_gradient_descent(w1,w2,P,ID)
-        random_example = P[select_random_example(len(P))]
-        w1,w2 = calculate_weights(w1,w2,ID,random_example,n)
-        cost_functions.append(cost_function)
-    return cost_functions
+
+        cost_tr = sto_gradient_descent(w1,w2,train_samples,train_tau)
+        cost_train.append(cost_tr)
+        cost_tst = sto_gradient_descent(w1,w2,test_samples,test_tau)
+        cost_test.append(cost_tst)
+        random_example = select_random_example(len(train_samples))
+        train_sample = train_samples[random_example]
+        random_tau = train_tau[random_example]
+        w1,w2 = calculate_weights(w1,w2,train_sample,random_tau, n)
+    return cost_train,cost_test
 
 
 def plot_different_parameters():
@@ -77,10 +83,17 @@ def plot_different_parameters():
     N = 50
 
     xi, tau = load_dataset()
+    tau_flat = tau[0]
     train_samples = xi[0:P] # First P examples for training
+    train_tau = tau_flat[0:P]
+    test_tau = tau_flat[P:P+Q]
     test_samples = xi[P:P+Q] # Q examples after that for testing
-
-    result = seq_training(train_samples, tau, P, n, N)
+    print(train_samples.shape)
+    print(test_samples.shape)
+    print(tau_flat.shape)
+    print(train_tau.shape)
+    print(test_tau.shape)
+    result_train,result_test = seq_training(train_samples,test_samples, train_tau,test_tau,  P, n, N)
 
 
 
@@ -100,15 +113,12 @@ def plot_different_parameters():
     #     print(float(succes_counter / counter))
     #     Qts.append(float(succes_counter / counter))
 
-    plt.plot(result)
+    plt.plot(result_train)
+    plt.plot(result_test)
     plt.ylabel(' ')
     plt.xlabel(' ')
     plt.title('Cost vs time')
     plt.show()
-
-
-def main():
-    plot_c()
 
 
 if __name__ == '__main__':
